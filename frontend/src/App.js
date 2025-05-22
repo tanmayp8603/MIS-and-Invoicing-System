5e1751c95b060ccc40d45003d5b4a257b897d8ba
tanmayp8603 <tanmaypatil285@gmail.com>
2025-05-22 13:21:38 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tanmay Patil
         </a>
       </header>
     </div>
